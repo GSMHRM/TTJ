d21b892d4111c44ca0e5ab17aa150ef5cec8e7b9
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-17 21:04:01 +0900

    초기 커밋
    커밋

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/AnythingBackApplication.java
@@ -0,0 +1,13 @@
+package com.gsmhrm.anything_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnythingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnythingBackApplication.class, args);
+    }
+
+}
---
+++ Anything_back/src/test/java/com/gsmhrm/anything_back/AnythingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsmhrm.anything_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnythingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
