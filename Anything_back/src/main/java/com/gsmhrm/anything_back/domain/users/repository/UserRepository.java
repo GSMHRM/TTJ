aede3c0e6df90ce6858150cb439c328a94199b8d
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-18 11:19:33 +0900

    오류 수정

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/users/repository/UserRepository.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/users/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.gsmhrm.anything_back.domain.users.repository;
 
 import com.gsmhrm.anything_back.domain.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String > {
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findUserByEmail(String email);
-    Boolean ExistByEmail(String email);
+    Boolean existsByEmail(String email);
 }
