63b3df71b8b463e5ffbc51b36c2c1c4183e22ab6
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-18 11:21:16 +0900

    가입 로그인 간단히 처리

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/users/presentation/UserController.java
@@ -0,0 +1,35 @@
+package com.gsmhrm.anything_back.domain.users.presentation;
+
+import com.gsmhrm.anything_back.domain.users.presentation.dto.SignInRequest;
+import com.gsmhrm.anything_back.domain.users.presentation.dto.SignUpRequest;
+import com.gsmhrm.anything_back.domain.users.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequest signupRequest) throws Exception {
+        userService.signUp(signupRequest);
+        return ResponseEntity.ok().body("회원가입이 성공했습니다.");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> signIn(@RequestBody SignInRequest signinRequest) throws Exception {
+
+        String token = userService.login(signinRequest);
+
+        return ResponseEntity.ok().body(token);
+    }
+
+}
