fa28002adddc7aa8a51fd917999bd533f7b57476
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-18 09:30:00 +0900

    이메일로 유저 검색, 이메일 인증 여부

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/users/repository/UserRepository.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/users/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.gsmhrm.anything_back.domain.users.repository;
 import com.gsmhrm.anything_back.domain.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String > {
 
-
+    Optional<User> findUserByEmail(String email);
+    Boolean ExistByEmail(String email);
 }
