4312c4c27b8aa8270eb1527acdeb37a4ea8df5e8
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-19 21:07:02 +0900

    어노테이션 추가

--- Anything_back/src/main/java/com/gsmhrm/anything_back/AnythingBackApplication.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/AnythingBackApplication.java
@@ -2,12 +2,21 @@ package com.gsmhrm.anything_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class AnythingBackApplication {
 
     public static void main(String[] args) {
