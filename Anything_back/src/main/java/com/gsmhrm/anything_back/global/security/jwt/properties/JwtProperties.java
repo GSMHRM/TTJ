36a8f3ef24129b06addb0c28db43f80c217db504
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-19 21:07:13 +0900

    JwtProperties

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/security/jwt/properties/JwtProperties.java
@@ -0,0 +1,14 @@
+package com.gsmhrm.anything_back.global.security.jwt.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final String accessSecret;
+    private final String refreshSecret;
+}
