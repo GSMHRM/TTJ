f9eb392fa52858fa2356d62a63248b86dfd6da9e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-20 17:45:18 +0900

    Access, Refresh token 생성

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/security/jwt/TokenProvider.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/security/jwt/TokenProvider.java
@@ -1,15 +1,22 @@
 package com.gsmhrm.anything_back.global.security.jwt;
 
 import com.gsmhrm.anything_back.global.security.auth.MemberDetailsService;
+import com.gsmhrm.anything_back.global.security.exception.TokenExpirationException;
+import com.gsmhrm.anything_back.global.security.exception.TokenNotValidException;
 import com.gsmhrm.anything_back.global.security.jwt.properties.JwtProperties;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Getter
 @Component
@@ -40,4 +47,57 @@ public class TokenProvider {
         return Keys.hmacShaKeyFor(bytes);
     }
 
+    private String generateToken(String userEmail, TokenType tokenType, String secret, long expireTime) {
+        final Claims claims = Jwts.claims();
+        claims.put(TokenClaimName.USER_EMAIL.value, userEmail);
+        claims.put(TokenClaimName.TOKEN_TYPE.value, tokenType);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expireTime))
+                .signWith(getSignkey(secret), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public Claims extractAllClaims(String token, String secret) {
+        token = token.replace("Bearer", "");
+
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(getSignkey(secret))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new TokenExpirationException();
+        } catch (JwtException e) {
+            throw new TokenNotValidException();
+        }
+    }
+
+    public ZonedDateTime getExpiredAtToken(String token, String secret) {
+        return ZonedDateTime.now().plusSeconds(ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    public String getUserEmail(String token, String secret) {
+        return extractAllClaims(token, secret).get(TokenClaimName.USER_EMAIL.value, String.class);
+    }
+
+    public String getTokenType(String token, String secret) {
+        return extractAllClaims(token, secret).get(TokenClaimName.TOKEN_TYPE.value, String.class);
+    }
+
+    public String generatedAccessToken(String email) {
+        return generateToken(email, TokenType.ACCESS_TOKEN, jwtProperties.getAccessSecret(), ACCESS_TOKEN_EXPIRE_TIME);
+    }
+
+    public String generatedRefreshToken(String email) {
+        return generateToken(email, TokenType.REFRESH_TOKEN, jwtProperties.getRefreshSecret(), REFRESH_TOKEN_EXPIRE_TIME);
+    }
+
+    public UsernamePasswordAuthenticationToken authenticationToken(String email) {
+        UserDetails userDetails = memberDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 }
