d26ad94be757920bc7acc6c82b14a273974ae4b4
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-20 17:44:40 +0900

    에러 코드 추가

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/ErrorCode.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
 
     EMAIL_ALREADY_EXIST("존재하는 이메일 입니다", 409),
     EMAIL_NOT_FOUND("존재하지 않는 이메일 입니다", 404),
-    WRONG_PASSWORD("잘못된 비밀번호 입니다", 400);
+    WRONG_PASSWORD("잘못된 비밀번호 입니다", 400),
+    TOKEN_IS_EXPIRED("토큰이 만료 되었습니다.", 401),
+    UNKNOWN_ERROR("알 수 없는 에러입니다.", 500),
+    TOKEN_NOT_VALID("토큰이 유효 하지 않습니다.", 401);
 
     private final String message;
     private final int status;
