e59031e33c7fe2f3826f15a03b7f0fb5b3761a4f
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-25 18:19:15 +0900

    쓸모없는거 제외

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/repository/RefreshTokenRepository.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/repository/RefreshTokenRepository.java
@@ -2,6 +2,7 @@ package com.gsmhrm.anything_back.domain.auth.repository;
 
 import com.gsmhrm.anything_back.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
 }
