e5e9b465702a66e077a67af186057abf89739663
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-09 19:34:39 +0900

    현재 로그인 된 유저

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/util/UserUtil.java
@@ -0,0 +1,20 @@
+package com.gsmhrm.anything_back.global.util;
+
+import com.gsmhrm.anything_back.domain.member.entity.Member;
+import com.gsmhrm.anything_back.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserUtil {
+
+    private final MemberRepository memberRepository;
+
+    public Member currentUser() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        return memberRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("로그인 안됨"));
+    }
+}
