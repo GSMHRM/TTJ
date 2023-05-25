864c8d7dc0e634412ae3b086bf6daec045d099c3
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-05-25 19:46:42 +0900

    cncv

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/presentation/AuthController.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/presentation/AuthController.java
@@ -2,40 +2,49 @@ package com.gsmhrm.anything_back.domain.auth.presentation;
 
 import com.gsmhrm.anything_back.domain.auth.presentation.dto.request.SignInRequest;
 import com.gsmhrm.anything_back.domain.auth.presentation.dto.request.SignUpRequest;
-import com.gsmhrm.anything_back.domain.auth.presentation.dto.response.LoginResponse;
-import com.gsmhrm.anything_back.domain.auth.service.impl.LogoutServiceImpl;
-import com.gsmhrm.anything_back.domain.auth.service.impl.SignInServiceImpl;
-import com.gsmhrm.anything_back.domain.auth.service.impl.SignupServiceImpl;
+import com.gsmhrm.anything_back.domain.auth.presentation.dto.response.NewTokenResponse;
+import com.gsmhrm.anything_back.domain.auth.presentation.dto.response.SignInResponse;
+import com.gsmhrm.anything_back.domain.auth.service.MemberLoginService;
+import com.gsmhrm.anything_back.domain.auth.service.MemberLogoutService;
+import com.gsmhrm.anything_back.domain.auth.service.MemberSignUpService;
+import com.gsmhrm.anything_back.domain.auth.service.NewTokenService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
-    private final SignupServiceImpl signupService;
-    private final SignInServiceImpl signInService;
-    private final LogoutServiceImpl logoutService;
+        private final MemberSignUpService signUpService;
+        private final MemberLoginService loginService;
+        private final MemberLogoutService logoutService;
+        private final NewTokenService newTokenService;
 
-    @PostMapping ("/users/join")
-    public ResponseEntity<HttpStatus> memberJoin(@RequestBody SignUpRequest request) {
-        signupService.execute(request);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+        @GetMapping("/signup")
+        public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequest request) {
+            signUpService.execute(request);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
 
-    @PostMapping("/users/login")
-    public ResponseEntity<LoginResponse> memberLogin(@RequestBody SignInRequest request) {
-        LoginResponse loginResponse = signInService.execute(request);
-        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
-    }
+        @PostMapping("/login")
+        public ResponseEntity<SignInResponse> login(@Valid @RequestBody SignInRequest request) {
+            SignInResponse response = loginService.execute(request);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
 
-    @DeleteMapping("/logout")
-    public ResponseEntity<HttpStatus> logout(@RequestHeader("Authorization")String accessToken){
-        logoutService.execute(accessToken);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        @DeleteMapping
+        public ResponseEntity<?> logout(@RequestHeader("Authorization")String accessToken) {
+            logoutService.execute(accessToken);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-    }
+        @PatchMapping
+        public ResponseEntity<NewTokenResponse> reIssueToken(@RequestHeader("RefreshToken") String token) {
+            NewTokenResponse newTokenResponse = newTokenService.execute(token);
+            return new ResponseEntity<>(newTokenResponse, HttpStatus.OK);
+        }
 }
--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/presentation/dto/request/SignUpRequest.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/presentation/dto/request/SignUpRequest.java
@@ -1,6 +1,5 @@
 package com.gsmhrm.anything_back.domain.auth.presentation.dto.request;
 
-import com.anything.s3.domain.member.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -23,5 +22,4 @@ public class SignUpRequest {
     private String name;
     @NotBlank(message = "비밀번호는 필수 입력값입니다")
     private String password;
-    private Role role;
 }
