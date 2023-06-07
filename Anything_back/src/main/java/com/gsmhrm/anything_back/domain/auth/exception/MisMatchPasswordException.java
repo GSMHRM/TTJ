793934f301a79fcba882d57b53bb85b12599ee88
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-07 19:52:22 +0900

    Exception 추가

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/exception/MisMatchPasswordException.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/auth/exception/MisMatchPasswordException.java
@@ -1,6 +1,6 @@
 package com.gsmhrm.anything_back.domain.auth.exception;
 
-import com.gsmhrm.anything_back.global.exception.handler.ErrorCode;
+import com.gsmhrm.anything_back.global.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
