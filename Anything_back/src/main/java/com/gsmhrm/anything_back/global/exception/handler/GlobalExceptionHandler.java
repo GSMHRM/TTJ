5f492fc99b314d297e54dafb6d17958e8518fda4
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-07 19:51:01 +0900

    커스텁 어노테

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.gsmhrm.anything_back.global.exception.handler;
+
+import com.gsmhrm.anything_back.global.exception.AnythingBackException;
+import com.gsmhrm.anything_back.global.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AnythingBackException.class)
+    public ResponseEntity<ErrorMessage> handleComprehensiveException(HttpServletRequest request, AnythingBackException e) {
+        printError(request, e.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode().getMessage(), e.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatusCode.valueOf(e.getErrorCode().getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
+        printError(request, "Request-Value is not Valid");
+        ErrorMessage errorMessage = new ErrorMessage("Request-Value is not Valid", HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    private void printError(HttpServletRequest request, String message) {
+        log.error(request.getRequestURI());
+        log.error(message);
+    }
+}
