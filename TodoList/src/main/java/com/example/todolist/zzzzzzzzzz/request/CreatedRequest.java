8bcc1e3b0aeef499033592211b96b3adec71686e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-14 20:49:52 +0900

    수정

--- TodoList/src/main/java/com/example/todolist/zzzzzzzzzz/request/CreatedRequest.java
+++ TodoList/src/main/java/com/example/todolist/zzzzzzzzzz/request/CreatedRequest.java
@@ -1,9 +1,11 @@
 package com.example.todolist.zzzzzzzzzz.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class CreatedRequest {
 
