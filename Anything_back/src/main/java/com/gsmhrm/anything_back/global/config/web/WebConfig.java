d2de75f97b2091760dab03515ad42a327cbfd56a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-20 19:38:50 +0900

    CORS

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/config/web/WebConfig.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/config/web/WebConfig.java
@@ -24,7 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:9040")
+                .allowedOrigins("http://localhost:3000",
+                                "http://localhost:9040",
+                                "https://localhost:3000",
+                                "https://notify-1.netlify.app/")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
