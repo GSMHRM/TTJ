d34acc282ce1de2ab7b379f7c36ab4222146c420
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-24 18:22:19 +0900

    get list

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/presentation/PlanController.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/presentation/PlanController.java
@@ -1,12 +1,14 @@
 package com.gsmhrm.anything_back.domain.plan.presentation;
 
 import com.gsmhrm.anything_back.domain.plan.presentation.dto.request.CreatePlanRequest;
+import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.PlanListResponse;
 import com.gsmhrm.anything_back.domain.plan.service.CreatePlanService;
 import com.gsmhrm.anything_back.domain.plan.service.ListPlanService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlanController {
 
     private final CreatePlanService createPlanService;
+    private final ListPlanService listPlanService;
 
     @PostMapping
     public ResponseEntity<Void> createPlan(@RequestBody @Valid CreatePlanRequest createPlanRequest) {
         createPlanService.execute(createPlanRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<PlanListResponse> getPlanList() {
+        var list = listPlanService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
