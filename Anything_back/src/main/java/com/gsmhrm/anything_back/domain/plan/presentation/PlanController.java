cc5593612a100ef147183daa1c8850e7dca2c19b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-24 18:30:08 +0900

    ListByTypeService

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/presentation/PlanController.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/presentation/PlanController.java
@@ -2,16 +2,15 @@ package com.gsmhrm.anything_back.domain.plan.presentation;
 
 import com.gsmhrm.anything_back.domain.plan.presentation.dto.request.CreatePlanRequest;
 import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.PlanListResponse;
+import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.TransTypeListResponse;
 import com.gsmhrm.anything_back.domain.plan.service.CreatePlanService;
 import com.gsmhrm.anything_back.domain.plan.service.ListPlanService;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("/plan")
 @RequiredArgsConstructor
@@ -29,6 +28,12 @@ public class PlanController {
     @GetMapping("/list")
     public ResponseEntity<PlanListResponse> getPlanList() {
         var list = listPlanService.execute();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/list/{type}")
+    public ResponseEntity<TransTypeListResponse> getPlanListByType(@PathVariable String type) {
+        var list = listPlanService.execute(type);
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 }
--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/service/ListPlanService.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/service/ListPlanService.java
@@ -1,8 +1,10 @@
 package com.gsmhrm.anything_back.domain.plan.service;
 
 import com.gsmhrm.anything_back.domain.plan.entity.Plan;
+import com.gsmhrm.anything_back.domain.plan.entity.enums.TypeTrans;
 import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.PlanListResponse;
 import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.PlanResponse;
+import com.gsmhrm.anything_back.domain.plan.presentation.dto.response.TransTypeListResponse;
 import com.gsmhrm.anything_back.domain.plan.repository.PlanRepository;
 import com.gsmhrm.anything_back.global.annotation.ReadOnlyService;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,18 @@ public class ListPlanService {
         List<Plan> planList = planRepository.findAll();
 
         return PlanListResponse.builder()
+                .planList(
+                        planList.stream()
+                                .map(PlanResponse::planResponse)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
+
+    public TransTypeListResponse execute(String transType) {
+        List<Plan> planList = planRepository.listByType(TypeTrans.valueOf(transType));
+
+        return TransTypeListResponse.builder()
                 .planList(
                         planList.stream()
                                 .map(PlanResponse::planResponse)
