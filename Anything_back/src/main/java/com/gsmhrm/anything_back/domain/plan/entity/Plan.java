406abb2f806008a68d7a07b6a1c734885b1d92ef
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-06-30 22:39:01 +0900

    asdf

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/entity/Plan.java
@@ -0,0 +1,36 @@
+package com.gsmhrm.anything_back.domain.plan.entity;
+
+import com.gsmhrm.anything_back.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "plan_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Boolean check;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime editTime;
+}
---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/plan/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.gsmhrm.anything_back.domain.plan.repository;
+
+import com.gsmhrm.anything_back.domain.plan.entity.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
