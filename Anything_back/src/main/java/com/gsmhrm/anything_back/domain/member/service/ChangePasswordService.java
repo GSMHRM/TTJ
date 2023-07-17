e68891278055bd9dc0b33caa0e1fa4037b5347f5
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-07-17 17:35:38 +0900

    🐛 ChangePasswordService 오류 수정

--- Anything_back/src/main/java/com/gsmhrm/anything_back/domain/member/service/ChangePasswordService.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/member/service/ChangePasswordService.java
@@ -5,6 +5,7 @@ import com.gsmhrm.anything_back.domain.member.entity.Member;
 import com.gsmhrm.anything_back.domain.member.presentation.dto.request.ChangePasswordRequest;
 import com.gsmhrm.anything_back.domain.member.repository.MemberRepository;
 import com.gsmhrm.anything_back.global.annotation.RollbackService;
+import com.gsmhrm.anything_back.global.util.UserUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
@@ -17,8 +18,8 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ChangePasswordService {
 
-    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserUtil util;
 
     @ApiResponses({
             @ApiResponse(code=204, message="비밀번호 변경 완료"),
@@ -27,12 +28,15 @@ public class ChangePasswordService {
     @ApiOperation(value = "비밀번호를 변경하는 메소드")
     public void execute(ChangePasswordRequest changePasswordRequest) {
 
-        Member member = memberRepository.findByPassword(passwordEncoder.encode(changePasswordRequest.getPassword()))
-                .orElseThrow(MisMatchPasswordException::new);
+        Member member = util.currentUser();
+
+        if (!passwordEncoder.matches(changePasswordRequest.getPassword(), member.getPassword())) {
+            throw new MisMatchPasswordException();
+        }
 
         if (!Objects.equals(changePasswordRequest.getWant_ps(), changePasswordRequest.getWant_ps_too())) {
             throw new MisMatchPasswordException();
-        }
+        }   
 
         member.changePassword(passwordEncoder.encode(changePasswordRequest.getWant_ps()));
     }
