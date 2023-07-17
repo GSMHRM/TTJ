d69339fffb60280928f771f7310fd1584df54a3f
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-07-17 16:50:58 +0900

    📝 Info 작성

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/domain/member/entity/MemberIntro.java
@@ -0,0 +1,25 @@
+package com.gsmhrm.anything_back.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class MemberIntro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    private String intro;
+}
