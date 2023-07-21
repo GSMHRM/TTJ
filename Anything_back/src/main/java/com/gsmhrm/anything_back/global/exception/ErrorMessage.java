c917d72564df4d080d6b7618958054fd6aa7809b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-07-21 20:52:11 +0900

    📝 DataIntegrityViolationException 작성

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/ErrorMessage.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/ErrorMessage.java
@@ -1,15 +1,29 @@
 package com.gsmhrm.anything_back.global.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class ErrorMessage {
     private final String message;
     private final int status;
 
     public ErrorMessage(ErrorCode errorCode){
         this(errorCode.getMessage(), errorCode.getStatus());
+    }
+
+    public static ResponseEntity<ErrorMessage> toResponseEntity(HttpStatus httpStatus, String message) {
+        return ResponseEntity
+                .status(httpStatus)
+                .body(ErrorMessage.builder()
+                        .status(httpStatus.value())
+                        .message(message)
+                        .build()
+                );
     }
 }
--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/handler/GlobalExceptionHandler.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,9 @@ import com.gsmhrm.anything_back.global.exception.AnythingBackException;
 import com.gsmhrm.anything_back.global.exception.ErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AnythingBackException.class)
@@ -27,6 +31,12 @@ public class GlobalExceptionHandler {
         printError(request, "Request-Value is not Valid");
         ErrorMessage errorMessage = new ErrorMessage("Request-Value is not Valid", HttpStatus.BAD_REQUEST.value());
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorMessage> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        log.error("handle DataIntegrityViolationException throw DataIntegrityViolationException");
+        return ErrorMessage.toResponseEntity(HttpStatus.CONFLICT, e.getMessage());
     }
 
     private void printError(HttpServletRequest request, String message) {
