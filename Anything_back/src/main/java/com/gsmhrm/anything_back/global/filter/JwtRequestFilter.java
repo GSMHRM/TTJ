0fc8a0434687df6cd6e5eb47979c23b8e5fae8df
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-16 22:47:50 +0900

    ♻️ JwtFilter

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/filter/JwtRequestFilter.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/filter/JwtRequestFilter.java
@@ -8,25 +8,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Logger;
 
 @Component
 @RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
-    private final JwtProperties jwtProperties;
 
-    public void registerSecurityContext(HttpServletRequest request, String email) {
-        UsernamePasswordAuthenticationToken authenticationToken = tokenProvider.authenticationToken(email);
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-    }
+    private final Logger log = (Logger) LoggerFactory.getLogger(JwtRequestFilter.class);
 
     @Override
     protected void doFilterInternal(
@@ -34,17 +33,18 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
-        String accessToken = request.getHeader("Authorization");
 
-        if (!Objects.isNull(accessToken)) {
-            tokenProvider.extractAllClaims(accessToken, jwtProperties.getAccessSecret());
+        String token = tokenProvider.resolveToken(request);
 
-            if (!tokenProvider.getTokenType(accessToken, jwtProperties.getAccessSecret()).equals("ACCESS_TOKEN")) {
-                throw new TokenNotValidException();
-            }
-            String email = tokenProvider.getUserEmail(accessToken, jwtProperties.getAccessSecret());
-            registerSecurityContext(request, email);
+        if (token != null && !token.isBlank()) {
+
+            Authentication authentication = tokenProvider.authenticationToken(token);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            log.info("current Email = " + authentication.getName());
         }
+        
         filterChain.doFilter(request, response);
     }
 }
