33bb5f0c510ea08d0dc6f32e8679b63b6592d015
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-17 15:37:40 +0900

    Log 오류 해결

--- Anything_back/src/main/java/com/gsmhrm/anything_back/global/filter/JwtRequestFilter.java
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/filter/JwtRequestFilter.java
@@ -1,31 +1,27 @@
 package com.gsmhrm.anything_back.global.filter;
 
-import com.gsmhrm.anything_back.global.security.exception.TokenNotValidException;
 import com.gsmhrm.anything_back.global.security.jwt.TokenProvider;
-import com.gsmhrm.anything_back.global.security.jwt.properties.JwtProperties;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.logging.Logger;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
 
     private final TokenProvider tokenProvider;
-
-    private final Logger log = (Logger) LoggerFactory.getLogger(JwtRequestFilter.class);
 
     @Override
     protected void doFilterInternal(
