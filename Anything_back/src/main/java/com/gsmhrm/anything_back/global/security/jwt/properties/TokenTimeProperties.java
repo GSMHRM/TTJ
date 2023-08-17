792aaf39bdad3547bd50c5deba12777d69ef5505
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-17 11:16:11 +0900

    TokenTimeProperties 작성

---
+++ Anything_back/src/main/java/com/gsmhrm/anything_back/global/security/jwt/properties/TokenTimeProperties.java
@@ -0,0 +1,15 @@
+package com.gsmhrm.anything_back.global.security.jwt.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class TokenTimeProperties {
+
+    private Long accessTime;
+
+    private Long refreshTime;
+}
