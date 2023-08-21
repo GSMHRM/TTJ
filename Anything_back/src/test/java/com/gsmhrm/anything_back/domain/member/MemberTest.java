b4ddbb4fd07ebe94b95e573459378b1a671dfe13
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-21 09:55:55 +0900

    🧪 MemberTest 실패작성

--- Anything_back/src/test/java/com/gsmhrm/anything_back/domain/member/MemberTest.java
+++ Anything_back/src/test/java/com/gsmhrm/anything_back/domain/member/MemberTest.java
@@ -1,13 +1,16 @@
 package com.gsmhrm.anything_back.domain.member;
 
+import com.gsmhrm.anything_back.domain.auth.exception.DuplicatedEmailException;
 import com.gsmhrm.anything_back.domain.member.entity.Member;
 import com.gsmhrm.anything_back.domain.member.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.Assertions;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -28,6 +31,31 @@ public class MemberTest {
 
         memberRepository.save(member);
 
-        Assertions.assertThat(memberRepository.findByEmail("zadzed1100@gmail.com").get().getName().equals("테스트이름"));
+        assertThat(memberRepository.findByEmail("zadzed1100@gmail.com").get().getName().equals("테스트이름"));
+    }
+
+    @Test
+    @DisplayName("유저 SignUp 실패 테스트")
+    void Member_save_Failed() {
+
+        Member member = Member.builder()
+                .email("zadzed1100@gmail.com")
+                .name("테스트이름")
+                .password("1234")
+                .build();
+
+        Member member2 = Member.builder()
+                .email("zadzed1100@gmail.com")
+                .name("테스트이름2")
+                .password("12345")
+                .build();
+
+        memberRepository.save(member);
+
+        System.out.println(member == member2);
+
+        Assertions.assertThrows(DuplicatedEmailException.class, () ->
+                memberRepository.save(member2)
+        );
     }
 }
